455205afa2e3a0465fe1c958504dabc3e5b70005
Javier78162 <javiergomezberprini@gmail.com>
2026-05-06 08:38:48 -0600

    cambios

--- Script.js
+++ Script.js
@@ -22,4 +22,23 @@ function mostrar_mensaje (){
      mensaje.style.transform = "transformateY/(0)";
   }, 50);
  }
- 
+ const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Guardar preferencia
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Cargar preferencia al iniciar
+window.addEventListener("load", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+});
